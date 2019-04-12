0f1b76813b52891153d8b952fdb29787e41506df
Ahmet Ensar Köprülü <31247645+ahmetkoprulu@users.noreply.github.com>
2019-04-12 19:16:49 +0300

    Api initial files added.

---
+++ src/Backend/DbContext.py
@@ -0,0 +1,115 @@
+import json
+
+from sqlalchemy import create_engine
+
+
+class InstructorDbContext:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def CreateInstructor(self, values):
+        connection = self.engine.connect()
+        with connection.begin() as trans:
+            connection.execute('INSERT INTO instructor (name, surname, email, password_hash) VALUES ({values})'
+                               .format(values=','.join(values)))
+
+    def SelectInstructorByCredentials(self, email, password_hash):
+        connection = self.engine.connect()
+        result = connection.execute("SELECT * FROM instructor WHERE email = '{email}' AND password_hash = '{password_hash}'".format(email=email, password_hash=password_hash))
+        connection.close()
+        print result
+        return json.dumps([dict(r) for r in result])
+
+
+class StudentDbContext:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def CreateStudent(self, values):
+        connection = self.engine.connect()
+        with connection.begin() as trans:
+            connection.execute("INSERT INTO student (name, surname, email, password_hash) VALUES ({values})"
+                               .format(values=','.join(values)))
+
+    def SelectInstructorByCredentials(self, email, password_hash):
+        connection = self.engine.connect()
+        result = connection.execute("SELECT * FROM student WHERE email = {email} AND password_hash = {password_hash}".format(email=email, password_hash=password_hash))
+        connection.close()
+        return result
+
+
+class ProjectDbContext:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def CreateProject(self, values):
+        connection = self.engine.connect()
+        with connection.begin() as trans:
+            connection.execute("INSERT INTO project (title, description, deadline, max_price, min_price, s_id) VALUES ({values})"
+                               .format(values=','.join(values)))
+
+
+class ProposalDbContext:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def CreateProposal(self, values):
+        connection = self.engine.connect()
+        with connection.begin() as trans:
+            connection.execute("INSERT INTO proposal (comment, price, p_id, i_id) VALUES ({values})"
+                               .format(values=','.join(values)))
+
+    def SelectProposalsByProjectId(self, id):
+        connection = self.engine.connect()
+        result = connection.execute("SELECT * FROM proposal WHERE p_id = {id}".format(id=id))
+        connection.close()
+        return json.dumps([(dict(row.items())) for row in result])
+
+
+class ContractDbContext:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def CreateContract(self, values):
+        connection = self.engine.connect()
+        with connection.begin() as trans:
+            connection.execute("INSERT INTO project (title, description, deadline, max_price, min_price, s_id) VALUES ({values})"
+                               .format(values=','.join(values)))
+
+
+class RateDbContext:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def CreateRate(self, values):
+        connection = self.engine.connect()
+        with connection.begin() as trans:
+            connection.execute("INSERT INTO rate (score, comment, date, c_id, s_id) VALUES ({values})"
+                               .format(values=','.join(values)))
+
+
+class DbContext:
+    engine = create_engine('postgres://postgres:ekrembaba1@localhost:5432/freelancer')
+
+    def __init__(self):
+        self.Instructors = InstructorDbContext(self.engine)
+        self.Students = StudentDbContext(self.engine)
+        self.Projects = ProjectDbContext(self.engine)
+        self.Proposals = ProposalDbContext(self.engine)
+        self.Proposals = ContractDbContext(self.engine)
+        self.Proposals = ProposalDbContext(self.engine)
+
+    def Execute(self, query):
+        connection = self.engine.connect()
+        result = connection.execute(query)
+        connection.close()
+        return result
+
+    def ExecuteWithRollback(self, table, columns, values):
+        connection = self.engine.connect()
+        print "INSERT INTO {table} ({columns}) VALUES ({values})".format(table=table, columns=', '.join(columns), values=','.join(values))
+        with connection.begin() as trans:
+            connection.execute("INSERT INTO {table} ({columns}) VALUES ({values})"
+                               .format(table=table, columns=', '.join(columns), values=','.join(values)))
+
+
---
+++ src/Backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+from DbContext import DbContext
+
+app = Flask(__name__)
+db = DbContext()
+
+@app.route('/')
+def hello_world():
+    return jsonify('sa'), 200
+
+
+@app.route('/instructors', methods=['POST'])
+def InstructorRegister():
+    content = request.get_json(force=True)
+    db.Instructors.CreateInstructor((content['name'], content['surname'], content['email'], content['password_hash']))
+    return content, 200
+
+
+@app.route('/instructors/login', methods=['POST'])
+def InstructorLogin():
+    content = request.get_json(force=True)
+    instructor = db.Instructors.SelectInstructorByCredentials(content['email'], content['password_hash'])
+    return instructor, 200
+
+
+@app.route('/students', methods=['POST'])
+def StudentRegister():
+    content = request.get_json(force=True)
+    db.Students.CreateStudent((content['name'], content['surname'], content['email'], content['password_hash']))
+    return content, 200
+
+@app.route('/proposals', methods=['POST'])
+def ProposalCreate():
+    content = request.get_json(force=True)
+    db.Proposals.CreateProposal((content['name'], content['surname'], content['email'], content['password_hash']))
+    return content, 200
+
+@app.route('/contracts', methods=['POST'])
+def ContractCreate():
+    content = request.get_json(force=True)
+    db.Proposals.CreateProposal((content['name'], content['surname'], content['email'], content['password_hash']))
+    return content, 200
+
+
+if __name__ == '__main__':
+    app.run(port=7000, debug=True)
