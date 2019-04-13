61df2cfae211181d7477cd77e489332f6b2cb628
Ahmet Ensar Köprülü <31247645+ahmetkoprulu@users.noreply.github.com>
2019-04-13 16:08:44 +0300

    Angular routing mechanism implemented

--- src/Frontend/src/app/app-routing.module.ts
+++ src/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'projects', component: ProjectComponent  },
+  {path: 'login', component: ProjectComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', redirectTo: 'register', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routing.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ProjectComponent } from './project/project.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    ProjectComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [
-    AppComponent
-  ]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      ProjectComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      RouterModule.forRoot(routes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
